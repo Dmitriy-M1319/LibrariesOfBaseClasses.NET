a3f33f9c8ac779def31062eed842f4d7b6d6c564
Дмитрий Мамонов <dimon.5642014@yandex.ru>
2020-08-25 11:01:50 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Dynamic;
+
+namespace LibrariesOfBaseClasses.NET
+{
+    class Program
+    {
+       static void PrintTime(object state)
+       {
+            Console.WriteLine($"Time is {DateTime.Now.ToLongDateString()}");
+       }
+        static void Main(string[] args)
+        {
+            TimerCallback tb = PrintTime;
+            Timer t = new Timer(tb, null, 0, 1000);
+            Console.ReadLine();
+            Console.WriteLine();
+            
+
+        }
+       
+
+
+    }
+}
